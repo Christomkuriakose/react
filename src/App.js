8b923962476a372a5e0a9abafe7a405ad129cc12
Christomkuriakose <129527137+Christomkuriakose@users.noreply.github.com>
2023-04-12 10:49:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>this is my 2 app</h1>
+      <h1>this is a demo</h1>
       <h1>good morning</h1>
     </div>
   );
